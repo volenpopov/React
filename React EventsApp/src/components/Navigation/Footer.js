c386e0c2900ddbf6abee9124dcffdcd3df9f8b18
volenpopov <volenpopov@abv.bg>
2020-01-15 22:58:33 +0200

    update footer to use themeContext and add button for switching between themes

--- React EventsApp/src/components/Navigation/Footer.js
+++ React EventsApp/src/components/Navigation/Footer.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ThemeContext from '../../context/theme-context';
+import themeIcon from '../../resources/themeIcon.png';
+import { THEMES_ARRAY } from '../../helpers/constants';
+import './Footer.css';
 
-const footer = () => {
+const Footer = () => {
+    const themeContext = useContext(ThemeContext);
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="bg-dark">
-            <div className="d-flex align-items-center" style={{height: '54px'}}>
-                <p className="m-0 py-2 pl-3 text-white">EventsApp {currentYear}&reg;</p>
+        <footer className={`bg-${themeContext.themeColor}`}>
+            <div className="d-flex justify-content-between align-items-center" style={{height: '54px'}}>
+                <p className={`m-0 py-2 pl-3 text-white`}>EventsApp {currentYear}&reg;</p>
+                <div>
+                    <p className="text-white d-inline">Theme </p>
+                    <img src={themeIcon} alt="ThemeIcon" onClick={() => {
+                        const indexOfCurrentlyActiveTheme = THEMES_ARRAY.indexOf(themeContext.themeColor);
+                        
+                        const indexOfNextTheme = indexOfCurrentlyActiveTheme < THEMES_ARRAY.length - 1
+                            ? indexOfCurrentlyActiveTheme + 1
+                            : 0;
+
+                        themeContext.switchTheme(THEMES_ARRAY[indexOfNextTheme]);
+                    }}/>
+                </div>
             </div>            
         </footer>
     );
 };
 
-export default footer;
+export default Footer;
