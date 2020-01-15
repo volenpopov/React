df530aef613d7f389c43eda7a7f5c36a9924b4c2
volenpopov <volenpopov@abv.bg>
2020-01-15 22:59:07 +0200

    add constants for theme colors

--- React EventsApp/src/helpers/constants.js
+++ React EventsApp/src/helpers/constants.js
@@ -1,7 +1,20 @@
-const PASSWORD_MIN_LENGTH = 5;
+const PASSWORD_MIN_LENGTH = 6;
 const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const REGISTER_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDd8TxVsQtDGbDpQ3-2bV9binkfDHRYbos';
+
+const DEFAULT_THEME = 'dark';
+const BLUE_THEME = 'primary';
+const GREEN_THEME = 'success';
+const RED_THEME = 'danger';
+const GRAY_THEME = 'secondary';
+const LIGHT_BLUE_THEME = 'info';
+
+const THEMES_ARRAY = [DEFAULT_THEME, BLUE_THEME, GREEN_THEME, RED_THEME, GRAY_THEME, LIGHT_BLUE_THEME];
 
 export {
     PASSWORD_MIN_LENGTH,
-    EMAIL_REGEX
+    EMAIL_REGEX,
+    REGISTER_URL,
+    DEFAULT_THEME,
+    THEMES_ARRAY
 };
