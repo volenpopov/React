54138d3416a749113f73efc5978cb268ea4927ed
volenpopov <volenpopov@abv.bg>
2020-02-15 20:38:15 +0200

    add ViewEvents btn

--- React EventsApp/src/pages/Home/HomeGuest.js
+++ React EventsApp/src/pages/Home/HomeGuest.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import ThemeContext from '../../context/theme-context';
 
 import './HomeGuest.css';
 
 const HomeGuest = () => {
+    const themeContext = useContext(ThemeContext);
+
     return (
         <div className="homeGuestContainer">
             <p>Welcome to EventsApp!</p>
-            <p>Please, create an account or login, in order to proceed.</p>
+            <p>Please, create an account or log in, in order to create or book an event.</p>
+            <Link to="/events" className={`btn btn-${themeContext.themeColor}`}>View Events</Link>
         </div>
     );
 };
