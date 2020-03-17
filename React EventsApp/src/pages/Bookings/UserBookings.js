8f98b7db848b51eeb51158c13bb7b509faa8b7cd
volenpopov <volenpopov@abv.bg>
2020-03-17 23:02:58 +0200

    load event date and disbale cancel on bookings of events that have already passed

--- React EventsApp/src/pages/Bookings/UserBookings.js
+++ React EventsApp/src/pages/Bookings/UserBookings.js
@@ -12,16 +12,29 @@ const UserBookings = props => {
     
     useEffect(() => {
         if (props.userId) {
-            axios.get(`${constants.BOOKINGS_URL}.json?orderBy="userId"&equalTo="${props.userId}"`)
-                .then(response => {
-                    const data = response.data;
-                    
-                    if (data) {
-                        const fetchedBookings = Object.keys(data)
-                            .map(key => ({ id: key, ...data[key] }));
+            const userBookingsRequest = axios.get(`${constants.BOOKINGS_URL}.json?orderBy="userId"&equalTo="${props.userId}"`);
+            const eventsRequest = axios.get(`${constants.EVENTS_URL}.json`);
+        
+            Promise.all([userBookingsRequest, eventsRequest])
+                .then(([bookings, events]) => {
+                    let userBookings = bookings.data;
+                    const allEvents = events.data;
 
-                        setUserBookings(fetchedBookings);
-                    }
+                    if (userBookings) {
+                        userBookings = Object.keys(userBookings)
+                        .map(key => {
+                            const bookedEventId = userBookings[key].eventId;
+                            const bookedEvent = allEvents[bookedEventId];
+
+                            return { 
+                                id: key,
+                                ...userBookings[key],
+                                eventDate: bookedEvent.date
+                            };
+                        });
+
+                        setUserBookings(userBookings);
+                    }                
                 })
                 .catch(error => console.log(error));
         }
@@ -36,11 +49,17 @@ const UserBookings = props => {
             .catch(error => console.log(error));
     };
 
-    const parsedBookings = userBookings.map(booking => {   
+    const currentDateNumber = Date.parse(new Date());
+
+    const parsedBookings = userBookings.map(booking => {
+        const eventDateNumber = Date.parse(booking.eventDate);
+
         return (
             <div className="eventContainer" key={booking.id}>
                 <div className="d-flex flex-column align-items-center p-3">
-                    <p className="eventTitle text-capitalize">{booking.eventId}</p>
+                    <p className="eventTitle text-capitalize">
+                        {`${booking.eventId} - ${new Date(booking.eventDate).toLocaleDateString("en-GB", constants.DATE_AND_TIME_OPTIONS)}`}
+                    </p>
                     <p 
                         className={`bg-${themeContext.themeColor} text-white px-2 mb-0 rounded text-center`}
                         style={{ width: "162px" }}>
@@ -50,6 +69,7 @@ const UserBookings = props => {
                 <div>
                     <button 
                         className={`mr-3 btn btn-${themeContext.themeColor} px-sm-3 px-lg-4`}
+                        disabled={eventDateNumber < currentDateNumber}
                         onClick={() => cancelBookingHandler(booking.id)}>                        
                         Cancel
                     </button>
@@ -58,6 +78,8 @@ const UserBookings = props => {
         );
     });
 
+    console.log(userBookings);
+    
     return (
         <div className="pageHeaderContainer w-100 align-self-start d-flex flex-column align-items-center text-center mt-5">
             <h1 className="mb-4">Your Bookings:</h1> 
