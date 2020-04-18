a3774e0656942c5bbb7a24dceaadcb1ef23a4c47
volenpopov <volenpopov@abv.bg>
2020-04-18 12:09:22 +0300

    modify modal with error

--- React EventsApp/src/components/Modal/Modal.js
+++ React EventsApp/src/components/Modal/Modal.js
@@ -1,27 +1,37 @@
 import React, { useContext } from "react";
 
 import ThemeContext from "../../context/theme-context";
+import { DEFAULT_THEME } from "../../helpers/constants";
 
 import "./Modal.css";
 
 const Modal = props => {
     const themeContext = useContext(ThemeContext);
-
+    
     const { title, children, actionButtonText, closeModal, onFormSubmit, authenticated } = props;
 
     return (
         <div className="modalContainer">
-            <h2 className={`title bg-${themeContext.themeColor}`}>{title}</h2>
+            <h2 className={`title bg-${themeContext.themeColor || DEFAULT_THEME}`}>
+                {title}
+            </h2>
             <form className="p-3" onSubmit={(e) => e.preventDefault()}>
                 {children}
                 <div className="buttonContainer">
                     <button 
-                        className={`btn btn-${themeContext.themeColor}`}
+                        className={`btn btn-${themeContext.themeColor || DEFAULT_THEME}`}
                         onClick={closeModal}>Close</button>
-                    <button 
-                        className={`btn btn-${themeContext.themeColor}`}
-                        onClick={onFormSubmit}
-                        disabled={!authenticated || actionButtonText === "Booked"}>{actionButtonText}</button>
+                    {
+                        actionButtonText
+                            ? (
+                                <button 
+                                    className={`btn btn-${themeContext.themeColor}`}
+                                    onClick={onFormSubmit}
+                                    disabled={!authenticated || actionButtonText === "Booked"}>{actionButtonText}
+                                </button>
+                            )
+                            : null
+                    }                    
                 </div>
             </form>
         </div>
