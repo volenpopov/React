4fa9fdb0cc4cbda11ba9661e1c8328630619ca61
volenpopov <volenpopov@abv.bg>
2020-07-25 16:45:14 +0300

    refactor Carousel tests

--- React EventsApp/src/components/Carousel/Carousel.test.js
+++ React EventsApp/src/components/Carousel/Carousel.test.js
@@ -7,39 +7,54 @@ import ImageCarousel from "./Carousel";
 import defaultImage from "../../resources/noimagefound.jpg";
 
 describe("<Carousel/>", () => {
-    let wrapper;
+    let component;
 
     beforeEach(() => {
-        wrapper = shallow(<ImageCarousel/>);
+        component = shallow(<ImageCarousel/>);
     });
 
-    const defaultItem = (
-        <Carousel.Item key="default">
-            <img className="carouselImage d-block w-100" src={defaultImage} alt="EventImage"/>
-        </Carousel.Item>
-    );
+    it("should render", () => {
+        expect(component.exists()).toBeTruthy();
+    });
     
     it("should display only default image when no images are passed as props", () => {
-        expect(wrapper.find(Carousel.Item)).toHaveLength(1);
-        expect(wrapper.contains(defaultItem)).toEqual(true);        
+        expect(component.find(Carousel)).toHaveLength(1);
+        expect(component.find(Carousel.Item)).toHaveLength(1);
+
+        expect(
+            component
+                .find(Carousel.Item)
+                .someWhere(carouselItem => carouselItem.key() === "default"
+                    && carouselItem.children().first().hasClass("carouselImage")
+                    && carouselItem.children().first().props().src === defaultImage)
+        ).toEqual(true);        
     });
 
-    it("should have as many carousel items as number of images passed as props", () => {
-        const imagesSrcs = ["1", "2"];
-        wrapper.setProps({ images: imagesSrcs });
+    it("should have as many carousel items as number of images passed as props and parse them properly", () => {
+        const firstImageSrc = "1";
+        const secondImageSrc = "2";
 
-        expect(wrapper.find(Carousel.Item)).toHaveLength(imagesSrcs.length);
+        const imagesSrcs = [firstImageSrc, secondImageSrc];
 
-        expect(wrapper.contains(
-            <Carousel.Item key={0}>
-                <img className="carouselImage d-block w-100" src={imagesSrcs[0]} alt="EventImage"/>
-            </Carousel.Item> 
-        )).toEqual(true);
+        component.setProps({ images: imagesSrcs });
 
-        expect(wrapper.contains(
-            <Carousel.Item key={1}>
-                <img className="carouselImage d-block w-100" src={imagesSrcs[1]} alt="EventImage"/>
-            </Carousel.Item> 
-        )).toEqual(true);
+        expect(component.find(Carousel)).toHaveLength(1);
+        expect(component.find(Carousel.Item)).toHaveLength(imagesSrcs.length);
+
+        expect(
+            component
+                .find(Carousel.Item)
+                .filterWhere(carouselItem => carouselItem.key() === imagesSrcs.indexOf(firstImageSrc).toString()
+                    && carouselItem.children().first().hasClass("carouselImage")
+                    && carouselItem.children().first().props().src === firstImageSrc)
+        ).toHaveLength(1);
+        
+        expect(
+            component
+                .find(Carousel.Item)
+                .filterWhere(carouselItem => carouselItem.key() === imagesSrcs.indexOf(secondImageSrc).toString()
+                    && carouselItem.children().first().hasClass("carouselImage")
+                    && carouselItem.children().first().props().src === secondImageSrc)
+        ).toHaveLength(1);  
     })
 });
