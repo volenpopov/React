1e6b22f60c09b14110d8b208d438b1fccc2dd8b5
volenpopov <volenpopov@abv.bg>
2020-08-28 22:19:30 +0300

    add catching of errors for react errors

--- React EventsApp/src/hoc/withErrorHandler/withErrorHandler.js
+++ React EventsApp/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from "react";
 
+import * as constants from "../../helpers/constants";
+
 import Modal from "../../components/Modal/Modal";
 import Backdrop from "../../components/Backdrop/Backdrop";
 
@@ -8,45 +10,93 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
         constructor(props) {
             super(props);
 
-            this.state = { error: null };
-
-            this.reqInterceptor = axios.interceptors.request.use(req => {
-                this.setState({ error: null });
-                return req;
-            });
+            this.state = {
+                error: null,
+                hasError: false,
+                isNetworkError: false
+            };
 
             this.respInterceptor = axios.interceptors.response.use(resp => resp, error => {
-                this.setState({ error });
+                this.setState({
+                    error,
+                    hasError: true,
+                    isNetworkError: true
+                });
             });
         }
 
         componentWillUnmount() {
-            axios.interceptors.request.eject(this.reqInterceptor);
             axios.interceptors.response.eject(this.respInterceptor);
         }
 
+        static getDerivedStateFromError(error) {
+            // Update state so the next render will show the fallback UI.
+            return {
+                error,
+                hasError: true
+            };
+          }
+        
+        componentDidCatch(error, errorInfo) {
+            this.logError(error, errorInfo);
+        }
+
+        componentDidUpdate(prevProps, prevState) {
+            if (!prevState.isNetworkError && this.state.isNetworkError) {
+                this.logError(this.state.error, "");
+            }
+        }
+
+        logError = (error, errorInfo) => {
+            const userId = localStorage.getItem("userId");
+
+            axios.post(`${constants.ERRORS_URL}.json`, {
+                error: error.toString(),
+                errorInfo,
+                userId: userId || "",
+                createdAt: new Date()
+            });
+        }
+
         closeModalHandler = () => {
-            this.setState({ error: null });
+            this.setState({
+                error: null,
+                hasError: false,
+                isNetworkError: false
+            });
         }        
         
         render() {
+            const {
+                hasError,
+                isNetworkError
+            } = this.state;
+
             const errorMessage = "An error occurred, please, try again or contact the support team.";
             
             const errorModal = (
                 <div>
                     <Backdrop/>
                     <Modal title="Error" closeModal={this.closeModalHandler}>
-                        <p>{ this.state.error ? errorMessage : null }</p>
+                        <p>{ errorMessage }</p>
                     </Modal>
                 </div> 
             );            
 
-            return (
-                <Fragment>
-                    { this.state.error ? errorModal : null}                    
-                    <WrappedComponent { ...this.props }/>
-                </Fragment>
-            );
+            let content = <WrappedComponent { ...this.props }/>;
+
+            if (hasError && isNetworkError) {
+                content = (
+                    <Fragment>
+                        { errorModal }                    
+                        <WrappedComponent { ...this.props }/>
+                    </Fragment>
+                );
+            } else if (hasError && !isNetworkError) {
+                content = <h1>Something went wrong</h1>;
+            }
+
+            return content;
         }
     }
 }
